365779f1ccbc516f53ba32a5b26736fef17e868d
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-12 14:10:28 +0100

    getting startet with dependency injection

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,6 +1,9 @@
-import { NewTaskData } from './task/task.model';
+import { Injectable } from '@angular/core';
 
-class TasksService {
+import { type NewTaskData } from './task/task.model';
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
   private tasks = [
     {
       id: 't1',
