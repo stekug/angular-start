eaceeab0e6d78de895029ad19cf5f0ad0deb56e6
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-14 16:18:08 +0100

    formatting output with a pipe

--- 03_EssentialsPractice/src/app/investment-results/investment-results.component.ts
+++ 03_EssentialsPractice/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 import { type InvestmentResults } from './investment-results.mode';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
