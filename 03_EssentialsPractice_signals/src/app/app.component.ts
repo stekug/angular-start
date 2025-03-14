57b90502d3252201b85ee5aecba46369e8687db6
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-14 16:21:17 +0100

    add signals variation

---
+++ 03_EssentialsPractice_signals/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+
+import { type InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { InvestmentResults } from './investment-results/investment-results.mode';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+})
+export class AppComponent {
+  resultsData?: InvestmentResults[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, duration, expectedReturn, annualInvestment } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    this.resultsData = annualData;
+  }
+}
---
+++ 03_EssentialsPractice_signals/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+import { type InvestmentResults } from './investment-results.mode';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  @Input() results?: InvestmentResults[];
+}
