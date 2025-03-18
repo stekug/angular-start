e7728ece84faf701d8186244e4b92a58c0a74669
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-18 17:39:29 +0100

    introduce the component lifecycle ngOnInit

--- 06_Compoents_Templates/src/app/dashboard/server-status/server-status.component.ts
+++ 06_Compoents_Templates/src/app/dashboard/server-status/server-status.component.ts
@@ -10,7 +10,23 @@ import { Component } from '@angular/core';
 export class ServerStatusComponent {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
+  // Its not wrong to do something like this, but there is another way, (Component Lifecycle)
+  // constructor() {
+  //   setInterval(() => {
+  //     const rnd = Math.random();
+
+  //     if (rnd < 0.33) {
+  //       this.currentStatus = 'online';
+  //     } else if (rnd < 0.66) {
+  //       this.currentStatus = 'offline';
+  //     } else {
+  //       this.currentStatus = 'unknown';
+  //     }
+  //   }, 5000);
+  // }
+
+  // This would be the better way:
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
 
