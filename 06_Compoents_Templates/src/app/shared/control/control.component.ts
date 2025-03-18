73e5b1ce8ae6b26ebc977964e0a94c34ccbd90d8
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-18 10:46:22 +0100

    install prettier

--- 06_Compoents_Templates/src/app/shared/control/control.component.ts
+++ 06_Compoents_Templates/src/app/shared/control/control.component.ts
@@ -1,11 +1,11 @@
-import { Component, input } from '@angular/core';
+import { Component, input } from "@angular/core";
 
 @Component({
-  selector: 'app-control',
+  selector: "app-control",
   standalone: true,
   imports: [],
-  templateUrl: './control.component.html',
-  styleUrl: './control.component.css',
+  templateUrl: "./control.component.html",
+  styleUrl: "./control.component.css",
 })
 export class ControlComponent {
   label = input.required<string>();
