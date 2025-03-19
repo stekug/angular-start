215b9538ebd583837190417ede1cf4af2445ac4e
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-19 09:28:31 +0100

    component cleanup with ngOnDestroy

--- 06_Compoents_Templates/src/app/dashboard/server-status/server-status.component.ts
+++ 06_Compoents_Templates/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() {}
 
   // Its not wrong to do something like this, but there is another way, (Component Lifecycle)
   // constructor() {
@@ -39,5 +42,13 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('After view init');
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
