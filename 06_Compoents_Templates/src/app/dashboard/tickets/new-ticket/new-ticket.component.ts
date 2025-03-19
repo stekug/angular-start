c4038f619de24858bfebedad348885877c190cad
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-19 12:01:45 +0100

    handling form submission

--- 06_Compoents_Templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06_Compoents_Templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit() {
+    console.log('Submitted!');
+  }
+}
