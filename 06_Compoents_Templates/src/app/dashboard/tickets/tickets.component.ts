de2a6a3a9ea8fa324092c86fba9fdb00b96ab0ac
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-19 15:37:10 +0100

    cross-component communication and state management

--- 06_Compoents_Templates/src/app/dashboard/tickets/tickets.component.ts
+++ 06_Compoents_Templates/src/app/dashboard/tickets/tickets.component.ts
@@ -24,4 +24,13 @@ export class TicketsComponent {
 
     this.tickets.push(newTicket);
   }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' };
+      }
+      return ticket;
+    });
+  }
 }
