2e9ae09ca4c2e544493d2832c53fb175a1ed9c3b
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-20 15:20:35 +0100

    build a pipe that sorts

--- 08_Pipes/src/app/app.component.ts
+++ 08_Pipes/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { TemperaturPipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [DatePipe, DecimalPipe, TemperaturPipe],
+  imports: [DatePipe, DecimalPipe, TemperaturPipe, SortPipe],
 })
 export class AppComponent {
   currentDate = new Date();
