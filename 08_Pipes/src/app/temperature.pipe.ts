e5858f2f1b2ccbeaa278b087f72f162f0fef86d9
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-20 14:54:37 +0100

    use custom pipe to perform custom transformations

--- 08_Pipes/src/app/temperature.pipe.ts
+++ 08_Pipes/src/app/temperature.pipe.ts
@@ -5,7 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturPipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    return value + ' - transformed';
+  transform(value: string | number) {
+    let val: number;
+
+    // Check if value is string, if - transform it
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    const outputTemp = val * (9 / 5) + 32;
+
+    return `${outputTemp} °F`;
   }
 }
