e270d91b9210e2517bbae6d7a068f014a78195e2
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-21 10:20:00 +0100

    using angulars dependency injection

--- 09_Services/src/app/tasks/new-task/new-task.component.ts
+++ 09_Services/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,6 +11,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  constructor(private tasksService: TasksService) {}
 
   onAddTask(title: string, description: string) {
     this.formEl()?.nativeElement.reset();
