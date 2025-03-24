1f29e56bd55016d0c6e0350e423254d69404c7f5
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-24 15:12:12 +0100

    showing a loading fallback and spinner

--- 12_HTTP/src/app/places/available-places/available-places.component.ts
+++ 12_HTTP/src/app/places/available-places/available-places.component.ts
@@ -5,20 +5,24 @@ import { type Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { map } from 'rxjs';
+import { SpinnerComponent } from '../../shared/spinner/spinner.component';
 
 @Component({
   selector: 'app-available-places',
   standalone: true,
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+  imports: [PlacesComponent, PlacesContainerComponent, SpinnerComponent],
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(map((resData) => resData.places))
@@ -26,6 +30,9 @@ export class AvailablePlacesComponent implements OnInit {
         next: (places) => {
           console.log(places);
           this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
     this.destroyRef.onDestroy(() => {
