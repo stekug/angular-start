ccb5b559d5b62f65bba259f05f2a79271a64effc
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-24 13:01:57 +0100

    converting observables to signals

--- 11_Observables/src/app/app.component.ts
+++ 11_Observables/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 import { interval, map } from 'rxjs';
 
@@ -18,6 +18,9 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit {
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+
   private destroyRef = inject(DestroyRef);
 
   constructor() {
