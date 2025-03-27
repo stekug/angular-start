c5352a00ca9fe5c900728da2e8774e78c417fff2
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-27 07:47:01 +0100

    setup new form

--- 13_HandlingForms/src/app/auth/signup/signup.component.ts
+++ 13_HandlingForms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
+  imports: [ReactiveFormsModule],
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  onSubmit() {
+    console.log('submit');
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
