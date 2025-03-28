247b3ff8a8a3bfbb3b61d9a1d8604599fe6a2624
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-28 13:52:30 +0100

    redirecting users

--- 14_Routing/src/app/app.routes.ts
+++ 14_Routing/src/app/app.routes.ts
@@ -16,6 +16,11 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix',
+      },
+      {
         path: 'tasks',
         component: TasksComponent,
       },
