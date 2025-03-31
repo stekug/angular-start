e450159f3d0874bf8c6ac6fe2d9a5edc9d966908
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-31 10:06:56 +0200

    using query params for data manipulation

--- 14_Routing/src/app/tasks/tasks.component.ts
+++ 14_Routing/src/app/tasks/tasks.component.ts
@@ -5,6 +5,7 @@ import {
   inject,
   input,
   OnInit,
+  signal,
 } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
@@ -20,20 +21,29 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 })
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
+  // This would be much "newer" and shorter, input binding:
   // order = input<'asc' | 'dsc'>();
-  order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   private tasksService = inject(TasksService);
   userTasks = computed(() =>
     this.tasksService
       .allTasks()
-      .filter((task) => task.userId === this.userId()),
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      }),
   );
+
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => (this.order = params['order']),
+      next: (params) => this.order.set(params['order']),
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
